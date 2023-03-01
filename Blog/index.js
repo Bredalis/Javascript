01243fdf18caca036ffc6a3ab766f146829951b2
PRO X <bredalisgautreaux@gmail.com>
2023-02-28 21:53:09 -0400

    Carpeta Actualizada

---
+++ Blog/index.js
@@ -0,0 +1,12 @@
+
+$(function(){
+
+	$("body").hide().fadeIn(1000)
+});
+
+function Estilo(){
+
+	var Cuerpo = document.getElementById("body");
+
+	Cuerpo.style.backgroundColor = "black"; 
+}
---
+++ First_Page/Portafolio/index.js
@@ -0,0 +1,8 @@
+
+$(function(){
+
+	$("body").hide().fadeIn(2000)
+	
+	$("img").hide().fadeIn(3000)
+
+});
